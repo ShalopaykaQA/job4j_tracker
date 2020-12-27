36820a786d5e0d503674d32ee7eab409867dc339
Elvira Semenova <shalopayka@gmail.com>
2020-12-27 10:03:14 +0300

    3. Банковские переводы [#410718]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,8 +9,7 @@ public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        List<Account> accounts = new ArrayList<>();
-        users.putIfAbsent(user, accounts);
+        users.putIfAbsent(user, new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {
